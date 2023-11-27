3db268445314f000835def4d17f2206f12222691
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-27 17:44:35 +0000

    Add files via upload

---
+++ while.5.py
@@ -0,0 +1,13 @@
+again ="y"
+while again == "y":
+     x =int(input("enter number 1: "))
+     y = int(input("enter the number 2:")) 
+     r =x + y
+     print(r)
+     again = input("do you want to continue: (y/n)? : ")
+exit()
+     
+
+    
+
+
